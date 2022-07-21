a8b02ef7991287a45820c593eab60d490c6611f8
wakennedy <alexkenned@gmail.com>
2022-07-21 10:47:04 -0700

    halfway with mysql integration

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logging from './config/logging';
 import config from './config/config';
-import sampleRoutes from './routes/sample';
+import bookRoutes from './routes/book';
 
 const NAMESPACE = 'Server';
 const router = express();
@@ -37,7 +37,7 @@ router.use((req, res, next) => {
 });
 
 /** Routes go here */
-router.use('/api/sample', sampleRoutes);
+router.use('/books', bookRoutes);
 
 /** Error handling */
 router.use((req, res, next) => {
